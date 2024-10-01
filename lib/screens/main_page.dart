a3fd6f612bdbbd505e8d66a9778195ae337f7359
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-01 12:24:19 +0300

    add map on the main page

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -14,20 +16,22 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Main Page"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: MaterialButton(
-          height: 50,
-          minWidth: 300,
-          color: Colors.green,
-          onPressed: () {
-            addInDataBase();
-          },
-          child: const Text("Test Connection"),
+      appBar: AppBar(title: Text('OpenStreetMap')),
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter:
+              LatLng(51.509364, -0.128928), // Center the map over London
+          initialZoom: 9.2,
         ),
+        children: [
+          TileLayer(
+            // Display map tiles from any source
+            urlTemplate:
+                'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+            userAgentPackageName: 'com.example.app',
+            // And many more recommended properties!
+          ),
+        ],
       ),
     );
   }
